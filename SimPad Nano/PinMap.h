addfb44c69f2d454c933332df28e1872f7d2cac2
Handle <mail@bysb.net>
2020-04-22 16:32:26 +0800

    引入 SimPad Nano 引脚定义

---
+++ SimPad Nano/PinMap.h
@@ -0,0 +1,33 @@
+/*--------------------------------------------------------------------------
+PinMap.H
+Header file for SimPad Nano pinmap.
+****************************************
+**  Copyright (C)  Handle 2018-2020   **
+**  Web:       https://sim.bysb.net   **
+****************************************
+--------------------------------------------------------------------------*/
+
+#include "Public/CH552.H"
+
+#ifndef __PIN_MAP__
+#define __PIN_MAP__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+sbit WS2812_Data = P1^4; // WS2812 信号引脚
+
+sbit BT1 = P1^7; // 默认设置 Z
+sbit BT2 = P1^5; // 默认设置 X
+
+sbit BT3 = P1^6; // 默认设置 Ctrl+R
+
+// sbit BT4 = P1^6;//默认设置F1
+// sbit BT5 = P1^6;//默认设置F2
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
